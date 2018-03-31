cf08f521c456e063df8df4b510d6317b56fbfc62
thodson-usgs <thodson@usgs.gov>
2018-03-30 19:35:09 -0500

    Basic functions for pulling data from nwis, streamstats and waterqualitydata

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='data_retrieval',
+      version='0.1',
+      description='',
+      url='',
+      author='Timothy Hodson',
+      author_email='thodson@usgs.gov',
+      license='MIT',
+      packages=[''],
+      zip_safe=False)
