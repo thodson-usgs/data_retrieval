35f217343508ab7188cb7bdaecd1f0f2cff27cdd
Timothy Hodson <thodson@usgs.gov>
2018-04-02 12:23:18 -0500

    added test

---
+++ tests/nwis_tests.py
@@ -0,0 +1,15 @@
+import pytest
+from data_retrieval.nwis import get_records
+
+def test_measurements_service():
+    """Test measurement service
+    """
+    start = '2018-01-24'
+    end   = '2018-01-25'
+    service = 'measurements'
+    site = '03339000'
+    df = get_records(site, start, end, service=service)
+    return df
+
+def test_measurements_service_answer():
+    assert df['measurement_nu'] == 801
